6108b66be9b0aec363d0b474e07c0dab6baddb6b
devxa2602 <akshayrawate@gmail.com>
2019-10-07 14:58:08 +0000

    remove gitignore files

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter,Switch,Route}from "react-router-dom";
+import {HashRouter,Switch,Route}from "react-router-dom";
 import App from "../App"
 import Recipe from "./recipe"
 const Router=()=>{
 	return(
-	<BrowserRouter basename='/recipe-finder'>
+	<HashRouter basename='/recipe-finder'>
 		<Switch>
 			<Route path='/'  component={App} exact/>
 		   <Route path='/recipe/:id' component={Recipe}/>
         </Switch>	
-	</BrowserRouter>
+	</HashRouter>
 	)
 }
 export default Router;
